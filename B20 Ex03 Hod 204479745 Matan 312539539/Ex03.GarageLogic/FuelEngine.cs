fdaae47aa21655996141bc9987557158bb9cd0c3
Hodjy <62711261+Hodjy@users.noreply.github.com>
2020-05-26 19:23:27 +0300

    Update2

---
+++ B20 Ex03 Hod 204479745 Matan 312539539/Ex03.GarageLogic/FuelEngine.cs
@@ -0,0 +1,36 @@
+﻿namespace Ex03.GarageLogic
+{
+    public class FuelEngine : Engine
+    {
+        private eFuelType m_EngineFuelType;
+
+        public FuelEngine(float i_CurrentEnergy, float i_MaxEnergy, eFuelType i_FuelType) : base(i_CurrentEnergy,i_MaxEnergy)
+        {
+            m_EngineFuelType = i_FuelType;
+        }
+
+        public enum eFuelType
+        {
+            Octan98,
+            Octan96,
+            Octan95,
+            Soler,
+        }
+
+        public void Recharge(float i_AmountToRecharge, eFuelType i_FuelType)
+        {
+            if(i_FuelType == m_EngineFuelType)
+            {
+                rechargeEnergy(i_AmountToRecharge);
+            }
+        }
+
+        public eFuelType EngineFuelType
+        {
+            get
+            {
+                return m_EngineFuelType;
+            }
+        }
+    }
+}
